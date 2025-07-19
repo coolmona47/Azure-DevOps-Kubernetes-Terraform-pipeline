4be986436135e2977a7fcf2598a226f1454e77ea
coolmona47 <mohankumars260601@gmail.com>
2025-07-19 18:37:12 +0530

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -47,3 +47,7 @@ terraform {
     # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
   }
 }
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+}
