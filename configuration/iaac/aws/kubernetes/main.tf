5b9f2559fd11bc21a71c2630eaba714b13b1745c
coolmona47 <kumarkumarmohanmona@gmail.com>
2025-07-23 17:06:23 +0530

    Updated EKS module config with fixed subnets and node_groups

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -10,7 +10,7 @@ terraform {
     key    = "path/to/my/key" # Will be overridden from build
     region = "us-east-1"
   }
-  
+     
   # Move version constraints here (fixes the warning)
   required_providers {
     kubernetes = {
@@ -27,24 +27,17 @@ terraform {
 resource "aws_default_vpc" "default" {
 }
 
-# Remove version from provider block
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
 module "in28minutes-cluster" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.31.2"  # Compatible with Terraform 1.5.7
-  
+     
   cluster_name    = "in28minutes-cluster"
   cluster_version = "1.24"  # Supported version
-  
+     
   # CORRECT: Use 'subnet_ids' for version 18.x
   vpc_id     = aws_default_vpc.default.id
   subnet_ids = ["subnet-035bd89faaa4b160b", "subnet-0397545b2caac2cb1"] #CHANGE
-  
+     
   # CORRECT: EKS managed node groups for v18.x
   eks_managed_node_groups = {
     main = {
@@ -52,46 +45,18 @@ module "in28minutes-cluster" {
       min_size       = 3
       max_size       = 5
       desired_size   = 3
-      
+             
       # Additional required settings for v18.x
       ami_type       = "AL2_x86_64"
       capacity_type  = "ON_DEMAND"
     }
   }
-  
+     
   # Required for v18.x
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.in28minutes-cluster.cluster_name
-  depends_on = [module.in28minutes-cluster]
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.in28minutes-cluster.cluster_name
-  depends_on = [module.in28minutes-cluster]
-}
-
-# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
-# ServiceAccount needs permissions to create deployments 
-# and services in default namespace
-resource "kubernetes_cluster_role_binding" "example" {
-  metadata {
-    name = "fabric8-rbac"
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "default"
-    namespace = "default"
-  }
-}
 
 # Needed to set the default region
 provider "aws" {
